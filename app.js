a88792f0352bf8db6af017826f0dc5dc6f7b9175
CamiloLM <kmilo_16_16@hotmail.com>
2020-04-20 21:14:46 -0500

    Cabeceras

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoute');
 const filmRoutes = require('./routes/filmRoute');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors({origin: '*'}));
 
 app.use('/api/film', filmRoutes); //Rutas relacionadas con la música
 app.use('/api/user', userRoutes); //Rutas relacionadas con usuario
