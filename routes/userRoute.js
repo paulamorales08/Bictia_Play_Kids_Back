5d2172bc35ec627f6575b2a7e44c84fe8f7003b2
CamiloLM <kmilo_16_16@hotmail.com>
2020-04-28 20:37:37 -0500

    Eliminar de favoritos

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,5 +9,6 @@ app.get('/getProfiles/:id', user.getProfiles);
 app.post('/login', user.login);
 app.get('/getProfile/:id', user.getOneProfile);
 app.put('/addFavorite/:id', user.addFavorite);
+app.put('/delFavorite/:id', user.removeFavorite);
 
 module.exports = app;
